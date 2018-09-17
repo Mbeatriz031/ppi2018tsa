ed41880ed74c293723dcdb5d394856d79e3eb02d
20141074010479 <20141074010479@MC266444.ifrn.local>
2018-09-17 11:23:23 -0300

    Adicionando projetos ao repositório.

---
+++ agenda/src/agenda/servlet/AdicionarContatoServlet.java
@@ -0,0 +1,61 @@
+package agenda.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import agenda.daos.ContatoDAO;
+import agenda.models.Contato;
+
+@WebServlet("/adicionarContato")
+public class AdicionarContatoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String endereco = request.getParameter("endereco");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		
+		Calendar dataFinal = null;
+		try {
+			Date dataProvisoria = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataFinal = Calendar.getInstance();
+			dataFinal.setTime(dataProvisoria);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEmail(email);
+		contato.setEndereco(endereco);
+		contato.setDataNascimento(dataFinal);
+		
+		ContatoDAO dao = new ContatoDAO();
+		dao.inserir(contato);
+		
+		PrintWriter out = response.getWriter();
+		 
+		 out.println("<html>");
+		 out.println("<body>");
+		 out.println("<h1>O nome é " + nome + "</h1>");
+		 out.println("<h1>O endereço é " + endereco + "</h1>");
+		 out.println("</body>");
+		 out.println("</html>");
+		
+	}
+}
+
+
+
